0e9f3c8f272972b0db70e2632cc35dd0104b1a0d
Cortez Jesus <cortez_laloramos@hotmail.com>
2025-07-22 03:18:40 -0500

    Mantenimiento de usuarios desarrollado

--- src/main/java/com/example/demo/repositories/UsuarioRepository.java
+++ src/main/java/com/example/demo/repositories/UsuarioRepository.java
@@ -1,15 +1,20 @@
 package com.example.demo.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.entities.Usuario;
 
-public interface UsuarioRepository extends JpaRepository <Usuario, Long>{
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-Optional<Usuario> findByCorreo(String correo);
-boolean existsByCorreo(String correo);
+    Optional<Usuario> findByCorreo(String correo);
 
-    
+    boolean existsByCorreo(String correo);
+
+    @Query("select u from Usuario u where u.rol.id=1")
+    List<Usuario> traerUsuariosClientes();
+
 }
